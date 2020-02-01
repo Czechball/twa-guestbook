d81dd744b20ae3a15d708164b2bcf47709ce129d
Czechball <david@jenne.cz>
2020-02-01 21:13:36 +0100

    table styling

--- index.php
+++ index.php
@@ -12,3 +12,36 @@ foreach ($posts as $row) {
     print "<p>" . $row["name"] . "-" . $row["subject"] ."<br/>". $row["text"]. "</p>";
 }
 ?>
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+
+	<title>Czechball's Guestbook</title>
+	<meta charset="UTF-8">
+	<link rel="shortcut icon" type="image/ico" href="favicon.ico"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+	<body>
+		<h1>Guest book</h1>
+		<form method="POST" action="<?php echo $_["SCRIPT_NAME"]; ?>">
+			<table margin="0" padding="2">
+				<tr>
+					<td align=right>Jméno</td>
+					<td align=left><input type="text" name="name"></td>
+				</tr>
+				<tr>
+					<td align=right>Předmět</td>
+					<td align=left><input type="text" name="subject"></td>
+				</tr>
+				<tr>
+					<td align=right>Text</td>
+					<td align=left><textarea cols="50" rows="5"></textarea></td>
+				</tr>
+				<tr>
+					<td align=right></td>
+					<td align=left><input type="submit" name="add" value="Odeslat"></td>
+				</tr>
+			</table>
+		</form>
+	</body>
