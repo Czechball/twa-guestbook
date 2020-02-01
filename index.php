c04683e28b50cdb2ee2403311d268fc75a0f8856
Czechball <david@jenne.cz>
2020-02-01 20:37:52 +0100

    actually printing posts now

--- index.php
+++ index.php
@@ -4,7 +4,9 @@ require_once "credentials.php";
 global $gbook;
 $gbook = new gbook($host, $port, $dbname, $user, $pass);
 $gbook->addPost("Jmeno", "Predmet", "Text");
-$gbook->deletePost(1);
-$debug = $gbook->getPosts();
-var_dump($debug);
+//$gbook->deletePost(1);
+$posts = $gbook->getPosts();
+foreach ($posts as $row) {
+    print "<p>" . $row["name"] . "-" . $row["subject"] ."<br/>". $row["text"]. "</p>";
+}
 ?>
