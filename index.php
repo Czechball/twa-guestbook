bcfb458d89be294f7c7d89156954ddd3f3dbc8e1
Czechball <david@jenne.cz>
2020-02-01 19:43:06 +0100

    fixed dollar sign

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 require_once "gbook.class.php";
 require_once "credentials.php";
 global $gbook;
-gbook = new gbook($host, $port, $dbname, $user, $pass);
+$gbook = new gbook($host, $port, $dbname, $user, $pass);
 ?>
