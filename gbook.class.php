c85c7cfea0f83fc8dbfc4d8a0559edb00b2e0444
Czechball <david@jenne.cz>
2020-02-01 18:54:25 +0100

    new file

---
+++ gbook.class.php
@@ -0,0 +1,25 @@
+<?php
+class gbook
+{
+    public $conn;
+        /**
+         * Konstruktor se připojí k DB ASW
+         */
+    public function __construct($host, $port, $dbname, $user, $pass)
+    {
+        $dsn = "mysql:host=$host;dbname=$dbname;port=$port";
+        $options = array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        );
+        try
+            {
+                $this->conn = new PDO($dsn, $user, $pass, $options);
+            }
+        catch(PDOException $e)
+            {
+                echo "Nelze se připojit k MySQL: ";  echo $e->getMessage();
+            }
+    }
+}
+?>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
-
-
+require_once "gbook.class.php";
+global $gbook;
+gbook = new gbook("192.168.1.120", "3337", "", "", "");
 ?>
