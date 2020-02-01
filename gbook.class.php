ffcd6778625c8c6f1cbbaaf93b5a9940ae550993
Czechball <david@jenne.cz>
2020-02-01 19:53:01 +0100

    added addPost function

--- gbook.class.php
+++ gbook.class.php
@@ -21,5 +21,26 @@ class gbook
                 echo "Nelze se připojit k MySQL: ";  echo $e->getMessage();
             }
     }
+
+
+    public function addPost($name, $subject, $text)
+    {
+        $ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
+        try
+        {
+            $stmt = $this->conn->prepare("INSERT INTO `gbook_2020` (`id`, `name`, `subject`, `text`, `date`, `ip`) VALUES (NULL, :name, :subject, :text, NULL, :ip);");
+            $stmt->bindParam(':name', $name);
+            $stmt->bindParam(':subject', $subject);
+            $stmt->bindParam(':text', $text);
+            $stmt->bindParam(':ip', $ip);
+//            $stmt->bindParam(':datum',$datum);
+            $stmt->execute();
+        }
+        catch(PDOException $e)
+        {
+            echo $e->getMessage();
+    }
+
+}
 }
 ?>
