afa210afde64d459ae9a277fb6a89c347ec1a17a
Czechball <david@jenne.cz>
2020-02-01 19:53:09 +0100

    calling the function

--- index.php
+++ index.php
@@ -3,4 +3,5 @@ require_once "gbook.class.php";
 require_once "credentials.php";
 global $gbook;
 $gbook = new gbook($host, $port, $dbname, $user, $pass);
+$gbook->addPost("Jmeno", "Predmet", "Text");
 ?>
