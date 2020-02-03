bbdd080999b8fa891625c8e3e6c94578c176c4d7
Czechball <david@jenne.cz>
2020-02-03 12:43:50 +0100

    Michael's epic fix

--- addpost.php
+++ addpost.php
@@ -9,9 +9,9 @@ $subject = $_POST["subject"];
 $text = $_POST["text"];
 if(empty($name) || empty($subject) || empty($text))
 {
-	header('Location: ' . $_SERVER['HTTP_REFERER']. '?add=2');
+	header('Location: ' . split($_SERVER['HTTP_REFERER'],'?')[0] . '?add=2');
 } else {
 	$gbook->addPost($name, $subject, $text);
-	header('Location: ' . $_SERVER['HTTP_REFERER']. '?add=1');
+	header('Location: ' . split($_SERVER['HTTP_REFERER'],'?')[0] . '?add=1');
 }
 ?>
