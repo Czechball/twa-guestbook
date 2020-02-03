59170ac1afb2dff01213ca798703285a0d530ba8
Czechball <david@jenne.cz>
2020-02-03 15:09:19 +0100

    removed unnecessary comments

--- index.php
+++ index.php
@@ -4,9 +4,6 @@ require_once "gbook.class.php";
 require_once "credentials.php";
 global $gbook;
 $gbook = new gbook($host, $port, $dbname, $user, $pass);
-//echo $gbook->verifyAdmin("admin", "password");
-//$gbook->addPost("Jmeno", "Predmet", "Text");
-//$gbook->deletePost(1);
 ?>
 <!DOCTYPE html>
 <html lang="en-US">
