ba50ae4abde4a6923863052a77d2f9fe4db48646
Czechball <david@jenne.cz>
2020-02-03 05:03:15 +0100

    no more Ps n.2

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ $gbook = new gbook($host, $port, $dbname, $user, $pass);
 	<link rel="stylesheet" href="style.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta property="og:title" content="Czechball's Guestbook">
-	<meta property="og:description" content="An amazing website where you can rate your experience on Half-Life LambdaPosting">
+	<meta property="og:description" content="Amazing website where you can rate your experience on Half-Life LambdaPosting">
 	<meta property="og:image" content="https://gbook.lambdaposting.games/meta-thumb.png">
 	<meta property="og:url" content="gbook.lambdaposting.games">
 </head>
